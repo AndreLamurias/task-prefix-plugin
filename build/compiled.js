090b41e816ac4f5f7c3f2848acc3b21a598b5891
Andre Lamurias <a.lamurias@fct.unl.pt>
2025-07-23 16:51:11 +0100

    functional add note name to task with ! command

--- build/compiled.js
+++ build/compiled.js
@@ -2,27 +2,31 @@
   // lib/plugin.js
   var plugin = {
     // --------------------------------------------------------------------------------------
-    constants: {},
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#insertText
-    insertText: {},
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#noteOption
-    noteOption: {
-      "Baby's first Note Option command": {
-        check: async function(app, noteUUID) {
-          const noteContent = await app.getNoteContent({ uuid: noteUUID });
-          return /cool/i.test(noteContent.toLowerCase());
-        },
-        run: async function(app, noteUUID) {
-          await app.alert("You clicked the Baby's first Note Option command in a COOL note!");
-          console.debug("Special message to the DevTools console");
-        }
+    replaceText: {
+      check(app) {
+        return "keyword";
+      },
+      async run(app) {
+        const noteuuid = await app.context.noteUUID;
+        const note = await app.notes.find(noteuuid);
+        app.alert(`This task is named ${note.name}`);
+        const taskUUID = await app.context.taskUUID || "not in a task";
+        const task = await app.getTask(taskUUID);
+        return note.name + task.content;
       }
     },
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#replaceText
-    replaceText: {}
+    /*async eventOption(app, taskUUID) {
+      const task = await app.getTask(taskUUID);
+      const noteuuid = await app.context.noteUUID;
+      const note = await app.notes.find(noteuuid);
+      await app.updateTask(taskUUID, { content: note.name + task.content });
+      //task.content = note.name + task.content;
+    }*/
+    async taskOption(app, task) {
+      const noteuuid = await app.context.noteUUID;
+      const note = await app.notes.find(noteuuid);
+      await app.updateTask(task.uuid, { content: "[" + note.name + "] " + task.content });
+    }
     // There are several other entry points available, check them out here: https://www.amplenote.com/help/developing_amplenote_plugins#Actions
     // You can delete any of the insertText/noteOptions/replaceText keys if you don't need them
   };
